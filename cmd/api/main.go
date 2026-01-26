88445760e3fbac6278526a3729ec6292ca36d1f7
Jugnu Gill <jugnugill.12354@gmail.com>
2026-01-26 16:59:26 +0000

    style: Remove redundant comment about starting the server.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,20 +13,16 @@ import (
 )
 
 func main() {
-	// 1. Initialize Redis Client
 	rdb := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
 
-	// 2. Initialize our Limiter
-	// We allow 5 requests every 10 seconds
+	// We allow 5 requests every 10 seconds for limiter
 	l := limiter.NewLimiter(rdb, 5, 10*time.Second)
 
-	// 3. Define the HTTP Handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-		// Use the IP address as the identifier
 		ip := r.RemoteAddr
 		host, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err == nil {
@@ -52,7 +48,6 @@ func main() {
 		fmt.Fprint(w, "Request successful!")
 	})
 
-	// 4. Start the Server
 	fmt.Println("Server starting on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
